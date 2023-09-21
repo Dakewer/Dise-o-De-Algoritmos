88c772aa6b5fb2b75d29734d6a921331def70af1
David <david.cinco@outlook.com>
2023-09-20 21:42:38 -0600

    TAREA NUMEROS PRIMOS

---
+++ Evaluador de numeros.py
@@ -0,0 +1,12 @@
+import math
+print("inserte el numero a analizar \n")
+flag = False
+evaluar = int(input())
+for i in range (2, int(math.sqrt(evaluar)) + 1):
+    if evaluar % i == 0:
+        flag = True
+        break
+if flag: 
+    print("No es primo\n")
+else:
+    print("Es primo\n")
